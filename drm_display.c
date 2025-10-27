623916a218118f13b5f5ceddebdf19b9fd7defef
Matheus Castello <matheus@castello.eng.br>
2025-10-27 11:46:00 -0300

    drm_display: Trim trailing whitespace
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- drm_display.c
+++ drm_display.c
@@ -94,7 +94,7 @@ static int find_drm_device(struct drm_device *dev) {
     if (connector->encoder_id) {
         encoder = drmModeGetEncoder(dev->fd, connector->encoder_id);
     }
-    
+
     if (!encoder) {
         for (i = 0; i < resources->count_encoders; i++) {
             encoder = drmModeGetEncoder(dev->fd, resources->encoders[i]);
@@ -122,7 +122,7 @@ static int find_drm_device(struct drm_device *dev) {
         dev->crtc = drmModeGetCrtc(dev->fd, encoder->crtc_id);
     }
 
-    printf("Selected mode: %dx%d@%dHz\n", 
+    printf("Selected mode: %dx%d@%dHz\n",
            dev->mode.hdisplay, dev->mode.vdisplay, dev->mode.vrefresh);
 
     return 0;
@@ -138,7 +138,7 @@ static int create_framebuffer(struct drm_device *dev) {
     create_req.width = dev->mode.hdisplay;
     create_req.height = dev->mode.vdisplay;
     create_req.bpp = 32; // RGBA8888
-    
+
     ret = drmIoctl(dev->fd, DRM_IOCTL_MODE_CREATE_DUMB, &create_req);
     if (ret) {
         fprintf(stderr, "Error: Cannot create dumb buffer: %s\n", strerror(errno));
@@ -151,7 +151,7 @@ static int create_framebuffer(struct drm_device *dev) {
 
 
     // Create framebuffer
-    ret = drmModeAddFB(dev->fd, dev->mode.hdisplay, dev->mode.vdisplay, 
+    ret = drmModeAddFB(dev->fd, dev->mode.hdisplay, dev->mode.vdisplay,
                        24, 32, dev->pitch, dev->handle, &dev->fb_id);
     if (ret) {
         fprintf(stderr, "Error: Cannot create framebuffer: %s\n", strerror(errno));
@@ -167,7 +167,7 @@ static int create_framebuffer(struct drm_device *dev) {
         return -1;
     }
 
-    dev->map = mmap(0, dev->size, PROT_READ | PROT_WRITE, MAP_SHARED, 
+    dev->map = mmap(0, dev->size, PROT_READ | PROT_WRITE, MAP_SHARED,
                     dev->fd, map_req.offset);
     if (dev->map == MAP_FAILED) {
         fprintf(stderr, "Error: mmap failed: %s\n", strerror(errno));
@@ -186,33 +186,33 @@ static void scale_and_center_image(unsigned char *image_data, int img_width, int
     float scale_x = (float)fb_width / img_width;
     float scale_y = (float)fb_height / img_height;
     float scale = (scale_x < scale_y) ? scale_x : scale_y;
-    
+
     int scaled_width = (int)(img_width * scale);
     int scaled_height = (int)(img_height * scale);
-    
+
     // Center image
     int offset_x = (fb_width - scaled_width) / 2;
     int offset_y = (fb_height - scaled_height) / 2;
-    
-    printf("Scaling image from %dx%d to %dx%d, centered at %d,%d\n", 
+
+    printf("Scaling image from %dx%d to %dx%d, centered at %d,%d\n",
            img_width, img_height, scaled_width, scaled_height, offset_x, offset_y);
-    
+
     for (int y = 0; y < scaled_height; y++) {
         for (int x = 0; x < scaled_width; x++) {
             // Map from scaled coordinates to original image coordinates
             int src_x = (int)(x / scale);
             int src_y = (int)(y / scale);
-            
+
             // Make sure we don't go out of bounds of the original image
             if (src_x >= img_width) src_x = img_width - 1;
             if (src_y >= img_height) src_y = img_height - 1;
-            
+
             // Get pixel from original image (RGB)
             int src_idx = (src_y * img_width + src_x) * 3;
             unsigned char r = image_data[src_idx];
             unsigned char g = image_data[src_idx + 1];
             unsigned char b = image_data[src_idx + 2];
-            
+
             // Write pixel to framebuffer (ARGB8888)
             int dst_x = offset_x + x;
             int dst_y = offset_y + y;
@@ -227,34 +227,34 @@ static void scale_and_center_image(unsigned char *image_data, int img_width, int
 static int display_image(struct drm_device *dev, const char *image_path) {
     int img_width, img_height, channels;
     unsigned char *image_data;
-    
+
     // Load image
     image_data = stbi_load(image_path, &img_width, &img_height, &channels, 3);
     if (!image_data) {
         fprintf(stderr, "Error: Cannot load image %s\n", image_path);
         return -1;
     }
-    
+
     printf("Image loaded: %dx%d, %d channels\n", img_width, img_height, channels);
-    
+
     // Clear framebuffer
     memset(dev->map, 0, dev->size);
-    
+
     // Scale and copy image to framebuffer
     uint32_t *fb_data = (uint32_t *)dev->map;
-    scale_and_center_image(image_data, img_width, img_height, 
+    scale_and_center_image(image_data, img_width, img_height,
                           fb_data, dev->mode.hdisplay, dev->mode.vdisplay);
-    
+
     stbi_image_free(image_data);
-    
+
     // Set mode and display framebuffer
-    int ret = drmModeSetCrtc(dev->fd, dev->encoder->crtc_id, dev->fb_id, 
+    int ret = drmModeSetCrtc(dev->fd, dev->encoder->crtc_id, dev->fb_id,
                             0, 0, &dev->connector->connector_id, 1, &dev->mode);
     if (ret) {
         fprintf(stderr, "Error: Cannot set CRTC: %s\n", strerror(errno));
         return -1;
     }
-    
+
     return 0;
 }
 
@@ -289,36 +289,36 @@ static void cleanup_drm(struct drm_device *dev) {
 
 int main(int argc, char *argv[]) {
     struct drm_device dev = {0};
-    
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <image_path>\n", argv[0]);
         return 1;
     }
-    
+
     printf("Starting DRM image viewer...\n");
-    
+
     // Initialize DRM device
     if (find_drm_device(&dev) != 0) {
         return 1;
     }
-    
+
     // Create framebuffer
     if (create_framebuffer(&dev) != 0) {
         cleanup_drm(&dev);
         return 1;
     }
-    
+
     // Display image
     if (display_image(&dev, argv[1]) != 0) {
         cleanup_drm(&dev);
         return 1;
     }
-    
+
     printf("Image displayed. Press Enter to exit...\n");
     getchar();
-    
+
     cleanup_drm(&dev);
     printf("Cleanup completed.\n");
-    
+
     return 0;
 }
